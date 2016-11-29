b91c8a56afff544bc08e5426f06e8db7e94ea686
Valerie1296 <valeriya1296@gmail.com>
2016-11-29 09:39:19 +0300

    ex3

---
+++ ex3.py
@@ -0,0 +1,42 @@
+__author__ = 'student'
+
+import turtle
+
+def koch(l, n):
+    assert n >= 0
+    assert l >= 0
+    turtle.shape('turtle')
+    if n == 0:
+        turtle.forward(l)
+        return
+
+    for i in range(n):
+        koch(l/3, n-1)
+        turtle.left(60)
+        koch(l/3, n-1)
+        turtle.right(120)
+        koch(l/3, n-1)
+        turtle.left(60)
+        koch(l/3, n-1)
+        return
+
+def snowflake(l, n):
+    assert n >= 0
+    assert l >= 0
+    koch(l, n)
+    turtle.right(120)
+    koch(l, n)
+    turtle.right(120)
+    koch(l, n)
+    turtle.right(120)
+    return
+
+
+
+
+
+l = 200
+n = int(input())
+snowflake(l, n)
+turtle.mainloop()
+
